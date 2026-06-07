61d9b4eac9d4c29868a07974d10be686c99ae00e
Oscar Cogan <oscycog@gmail.com>
2026-06-07 16:11:00 +1000

    Determine parameter of user and random score

--- prac_02/scores.py
+++ prac_02/scores.py
@@ -3,12 +3,23 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+def main():
+    score = float(input("Enter score: "))
+    parameter = return_parameter(score)
+    print(f"User score {score} is {parameter}")
+    score = random.randint(0, 100)
+    parameter = return_parameter(score)
+    print(f"Random: {score} = {parameter}")
+
+def return_parameter(score):
+    if score < 0 or score > 100:
+        return "Invalid"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
