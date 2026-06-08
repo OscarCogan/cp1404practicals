55b981dc4b89a2e02693bd4304cca71576ab8205
Oscar Cogan <oscycog@gmail.com>
2026-06-08 13:02:19 +1000

    Answer line questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+# What did you see on line 1? What was the smallest number you could have seen, what was the largest?
+    # On line 1, I saw 15, 20, 20 and 16
+    # The smallest number I could have seen was 5 and the highest I could have seen was 20
+
+# What did you see on line 2? What was the smallest number you could have seen, what was the largest? Could line 2 have produced a 4?
+    # On line 2, I saw 9, 5, 3 and 9
+    # The smallest number I could have seen was 3 and the highest I could have seen was 9
+    # Line 2 could not have produced a 4. The listed step value was 2, meaning that the only values that could have been produced were 3, 5, 7 and 9
+
+# What did you see on line 3? What was the smallest number you could have seen, what was the largest?
+    # On line 3 I saw 3.294207928583011, 5.016136361954601, 3.888697611980593 and 5.166725328605076
+
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
