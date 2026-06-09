f0fd60ac41b6dc3509a520ae8b7d7a7f1d28f2b3
Oscar Cogan <oscycog@gmail.com>
2026-06-09 20:56:46 +1000

    Add number file and adding program

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,3 +9,11 @@ in_file = open("name.txt")
 name = in_file.read()
 in_file.close()
 print(f"Hi {name.strip()}!")
+
+# 3:
+sum_of_numbers = 0
+with open("numbers.txt") as in_file:
+    numbers = in_file.readlines()
+    for i in range(0, 2):
+        sum_of_numbers += int(numbers[i])
+    print(sum_of_numbers)
