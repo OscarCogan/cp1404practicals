459f1262b79bf42d034e5276d090f76a931bac47
Oscar Cogan <oscycog@gmail.com>
2026-06-09 20:43:20 +1000

    Add file reading program

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,9 @@ name = input("Enter a name: ")
 out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
+
+# 2:
+in_file = open("name.txt")
+name = in_file.read()
+in_file.close()
+print(f"Hi {name.strip()}!")
